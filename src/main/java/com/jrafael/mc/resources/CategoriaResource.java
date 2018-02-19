62794d9fa5a1064362a838cc28c6610d16c07966
Jose Rafael <valkyre7@gmail.com>
2018-02-19 21:15:21 +0000

    added rest resource

--- src/main/java/com/jrafael/mc/resources/CategoriaResource.java
+++ src/main/java/com/jrafael/mc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.jrafael.mc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jrafael.mc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest a bombar forte!!!";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1,"Cat1");
+		Categoria c2 = new Categoria(2,"Cat2");
+		
+		List<Categoria> catList = new ArrayList<>();
+		catList.add(c1);
+		catList.add(c2);
+		
+		return catList;
 	}
 }
