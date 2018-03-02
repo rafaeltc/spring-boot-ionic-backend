6be02b9646378d9753a01e74d3386d1dbcfa5845
Jose Rafael <valkyre7@gmail.com>
2018-03-02 19:09:30 +0000

    Added Categorias on application startup

--- src/main/java/com/jrafael/mc/CursomcApplication.java
+++ src/main/java/com/jrafael/mc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.jrafael.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.jrafael.mc.domain.Categoria;
+import com.jrafael.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null,"informatica");
+		Categoria cat2 = new Categoria(null,"escritorio");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
