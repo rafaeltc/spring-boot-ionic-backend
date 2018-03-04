29a90d5d7d9fc123193dc05f1e634489cadb3fd2
Jose Rafael <valkyre7@gmail.com>
2018-03-04 09:35:56 +0000

    Endpoint /clientes available

---
+++ src/main/java/com/jrafael/mc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.jrafael.mc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jrafael.mc.domain.Cliente;
+import com.jrafael.mc.repositories.ClienteRepository;
+import com.jrafael.mc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	public ClienteRepository repo;
+	
+	public Cliente getCliente(Integer id) {
+		Cliente cat = repo.findOne(id);
+		
+		if(cat == null) {
+			throw new ObjectNotFoundException("Object not found with id = " + id
+					+ ", Type = " + Cliente.class.getName());
+		}
+		
+		return cat;
+	}
+}
